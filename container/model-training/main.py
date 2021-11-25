9c3aca7f37392347eb7fef9a8c4a74a96b15de03
David Hundley <david.k.hundley@gmail.com>
2021-11-25 14:06:42 -0600

    Starting entrypoint script for model training

---
+++ container/model-training/main.py
@@ -0,0 +1,32 @@
+# Importing the necessary Python libraries
+import os
+import pandas as pd
+
+# Importing the helper functions from other adjacent files
+from perform_feature_engineering import *
+
+
+
+## PROJECT SUPPORT
+## ---------------------------------------------------------------------------------------------------------------------
+# Pointing to the primary directory where data will be loaded and saved
+PRIMARY_DIRECTORY = "/opt/ml/"
+
+# Noting the subdirectories underneath the primary directory
+INPUT_PATH = os.path.join(PRIMARY_DIRECTORY, 'input/data')
+MODEL_PATH = os.path.join(PRIMARY_DIRECTORY, 'models')
+OUTPUT_PATH = os.path.join(PRIMARY_DIRECTORY, 'output')
+
+# Loading in the CSV from the output of the data collection
+df_all_data = pd.read_csv(os.path.join(INPUT_PATH, 'all_data.csv'))
+
+
+
+## SCRIPT INSTANTIATION
+## ---------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    # Performing feature engineering on the raw dataset
+    df_clean = perform_feature_engineering(df_all_data, OUTPUT_PATH)
+    
+    print(df_clean.head())
+    print(df_clean.info())
